a9c1f2b9d0ee523705e76dadd7f9708673afae65
lookinkz <work@quana.kz>
2022-11-21 16:56:36 +0600

    12 lesson

---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <img
+        src="https://images.pexels.com/photos/12906178/pexels-photo-12906178.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt=""
+      />
+
+      <div>ava + description</div>
+
+      <div>
+        My post
+        <div>New Post</div>
+      </div>
+
+      <div>Posts</div>
+
+      <div>Posts 2</div>
+    </div>
+  );
+};
+
+export default Profile;
