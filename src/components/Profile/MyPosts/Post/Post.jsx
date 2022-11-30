83b99a741e27793b53e0987fe95351f8932fd24f
Ruslan Kuanyshbayev <r.kuanyshbayev@gmail.com>
2022-11-30 17:27:09 +0600

    16 done

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import s from "./Post.module.css";
 
-const Post = () => {
+const Post = (props) => {
   return (
     <div className={s.item}>
       <img
         src="https://www.hollywoodreporter.com/wp-content/uploads/2019/03/avatar-publicity_still-h_2019.jpg?w=1296"
         alt=""
       />
-      Post 1
+      {props.message}
       <div>
         <span>like</span>
       </div>
