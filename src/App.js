230b9ff238fff682d427c16d5342f2f5b91bbc68
Ruslan Kuanyshbayev <r.kuanyshbayev@gmail.com>
2022-12-05 03:11:33 +0600

    20 working on

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Profile from "./components/Profile/Profile";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Setting from "./components/Setting/Setting";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -18,6 +21,9 @@ const App = (props) => {
           <Routes>
             <Route path="/profile" element={<Profile />} />
             <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/setting" element={<Setting />} />
           </Routes>
         </div>
       </div>
