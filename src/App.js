a292e3a92f01fec270c436e3ab8fd488db0c0d45
lookinkz <work@quana.kz>
2022-12-04 17:55:57 +0600

    19 done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { Profiler } from "react";
 import "./App.css";
 
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Dialogs from "./components/Dialogs/Dialogs";
+import Profile from "./components/Profile/Profile";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = (props) => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div class="app-wrapper-content">
-        <Dialogs />
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/dialogs" element={<Dialogs />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
