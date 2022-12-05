bcc1bbbeb88baafe3a149681b74b9af88b2a6553
lookinkz <work@quana.kz>
2022-12-05 18:02:40 +0600

    21 working on

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog}>Ruslan</div>
+        <div className={s.dialog}>Ansar</div>
+        <div className={s.dialog}>Anvar</div>
+        <div className={s.dialog}>Janna</div>
+        <div className={s.dialog}>Jenny</div>
+      </div>
+      <div className="messages">
+        <div className="message">Hi!</div>
+        <div className="message">How are you?</div>
+        <div className="message">Yo</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import s from "./Navbar.module.css";
 import { NavLink } from "react-router-dom";
 
-const Navbar = (props) => {
+const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
