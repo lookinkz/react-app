c1c7ffd2c097c45635c7a1018c311b0755dad909
lookinkz <work@quana.kz>
2022-12-07 11:58:15 +0600

    21 done

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,16 +5,16 @@ const Dialogs = (props) => {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <div className={s.dialog}>Ruslan</div>
+        <div className={s.dialog + " " + s.active}>Ruslan</div>
         <div className={s.dialog}>Ansar</div>
         <div className={s.dialog}>Anvar</div>
         <div className={s.dialog}>Janna</div>
         <div className={s.dialog}>Jenny</div>
       </div>
-      <div className="messages">
-        <div className="message">Hi!</div>
-        <div className="message">How are you?</div>
-        <div className="message">Yo</div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi!</div>
+        <div className={s.message}>How are you?</div>
+        <div className={s.message}>Yo</div>
       </div>
     </div>
   );
